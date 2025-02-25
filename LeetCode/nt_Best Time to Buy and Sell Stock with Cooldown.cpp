ed5010ab64e0aaac5c15f8393072d69bc50f451b
MSZ-006NOC <msz006incubus@gmail.com>
2025-02-25 13:59:53 -0800

    update

---
+++ LeetCode/nt_Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // leetcode 309 with 冷却期
+        // tc O(n), sc O(n)
+        int n = prices.size();
+        if(n == 1) return 0;
+        if(n == 2){
+            int v = -prices[0] + prices[1];
+            return v > 0? v : 0; 
+        }
+
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        dp[1][0] = max(dp[0][0], -prices[1]);
+        dp[1][1] = max(dp[0][1], dp[0][0] + prices[1]);
+
+        for(int i = 2; i < n; ++i){
+            dp[i][0] = max(dp[i - 1][0], dp[i - 2][1] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        return dp[n - 1][1];
+    }
+};
+    
